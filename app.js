0e6d90d3b1c7605d52a0c77dc1d2256be14ef2f5
Stephen Zsolnai <steve@zolla.co.uk>
2013-08-04 20:06:21 +0100

    touch to trigger build

---
+++ app.js
@@ -0,0 +1,49 @@
+
+
+var express = require('express')
+  , engine = require('ejs-locals')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path')
+  , port = 2001;
+
+var app = express();
+
+app.engine('ejs', engine);
+
+app.configure(function(){
+
+  app.set('template_engine', 'ejs');
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.set('layout', 'layout');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(require('stylus').middleware({
+      src : __dirname,
+      dest : __dirname + '/public'
+    }));
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+app.get('/home', routes.home);
+
+
+app.listen(port);
+console.log('Express app started on port: ' + port);
+
+
+
+
+
+
+
